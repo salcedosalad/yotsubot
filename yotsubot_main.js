dc0e67c7fd2e46844a522458e1798b1d51b725c2
salcedosalad <mathewdavidsalcedo@gmail.com>
2021-01-09 14:21:36 -0800

    first commit

---
+++ yotsubot_main.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Yotsubot is online!');
+});
+
+//make sure this is executed last
+client.login('Nzk3NTgyODQ4MDUwNzI0OTE1.X_ok6Q.U3aHYy2dkQ34V0kw5f-Xg4Vqr5E');
