bc8d591195e1a2e4185cd90f56380b4b7cbb8a81
salcedosalad <74887339+salcedosalad@users.noreply.github.com>
2021-01-10 13:26:31 -0800

    Update yotsubot_main.js

--- yotsubot_main.js
+++ yotsubot_main.js
@@ -66,4 +66,5 @@ client.on('message', message => {
 });
 
 //make sure this is executed last
+//if on github, note this token is no longer valid
 client.login('Nzk3NTgyODQ4MDUwNzI0OTE1.X_ok6Q.U3aHYy2dkQ34V0kw5f-Xg4Vqr5E');
